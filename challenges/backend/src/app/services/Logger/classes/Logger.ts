2e6347c36699a364575764dca82752e948552cad
Hadrian Węgrzynowski <hadrian@hawski.com>
2019-08-03 14:43:46 +0200

    Remove empty constructor from Logger

--- challenges/backend/src/app/services/Logger/classes/Logger.ts
+++ challenges/backend/src/app/services/Logger/classes/Logger.ts
@@ -5,10 +5,6 @@ import "reflect-metadata";
 @injectable()
 export class Logger implements ILogger {
 
-    public constructor() {
-    }
-
-
     public log(message: string): void {
         console.log(`[LOG]: ${message}`);
     }
