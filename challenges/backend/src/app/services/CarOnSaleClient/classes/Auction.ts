ba5d7fb50503f3effef5957ebf245bbea0e8ae1e
Hadrian Węgrzynowski <hadrian@hawski.com>
2019-08-03 15:57:17 +0200

    Verify Auction object in constructor

--- challenges/backend/src/app/services/CarOnSaleClient/classes/Auction.ts
+++ challenges/backend/src/app/services/CarOnSaleClient/classes/Auction.ts
@@ -1,3 +1,4 @@
+import { equal, ok } from "assert";
 import "reflect-metadata";
 
 export class Auction {
@@ -9,5 +10,17 @@ export class Auction {
         this.numBids = auction.numBids || null;
         this.minimumRequiredAsk = auction.minimumRequiredAsk || null;
         this.currentHighestBidValue = auction.currentHighestBidValue || null;
+
+        ok(this.minimumRequiredAsk === null || this.minimumRequiredAsk >= 0,
+            "bad minimumRequiredAsk");
+        if(this.numBids === 0 || this.numBids === null) {
+            ok(this.currentHighestBidValue === null,
+                "expected null currentHighestBidValue");
+        } else {
+            ok(this.numBids >= 0,
+                "expected numBids >= 0");
+            ok(this.currentHighestBidValue > 0,
+                `expected positive currentHighestBidValue, actual: ${this.currentHighestBidValue}`);
+        }
     }
 }
