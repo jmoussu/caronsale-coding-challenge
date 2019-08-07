c9ec810e55151e691564d512768a1ac556fce518
Carlos Fuentes <metcdoder95@MacBook-Pro-de-Carlos.local>
2019-08-07 23:16:42 +0200

    fixed issues with Interfaces and Typos - APIClient

--- challenges/backend/src/app/services/APIClient/interfaces/IAPIClient.ts
+++ challenges/backend/src/app/services/APIClient/interfaces/IAPIClient.ts
@@ -1,18 +1,15 @@
 /**
  * This service describes an interface to access auction data from the CarOnSale API.
  */
-import { RequestConfiguration, APIError, APIResponse } from '../types';
+import { RequestConfiguration, APIResponse } from '../types';
 
 export default interface IAPIClient {
   setBaseUrl(baseURL: string): void;
   getClientHeaders(): object;
-  get(
-    url?: string,
-    config?: RequestConfiguration
-  ): Promise<APIResponse> | Promise<APIError>;
+  get(url?: string, config?: RequestConfiguration): Promise<APIResponse>;
   put(
     body: object,
     url?: string,
     config?: RequestConfiguration
-  ): Promise<APIResponse> | Promise<APIError>;
+  ): Promise<APIResponse>;
 }
--- challenges/backend/src/app/services/APIClient/types/index.ts
+++ challenges/backend/src/app/services/APIClient/types/index.ts
@@ -9,21 +9,3 @@ export type APIResponse = {
   data: any;
   status: number;
 };
-
-export type APIError = {
-  response?: APIResponse;
-  request?: any;
-  message?: string;
-};
-
-export type CallbackPreHookRequest = (
-  value: AxiosRequestConfig
-) => Promise<AxiosRequestConfig>;
-
-export type CallbackOnErrorRequest = (error: any) => Promise<any>;
-
-export type CallbackHookResponse = (
-  value: AxiosResponse
-) => Promise<AxiosResponse>;
-
-export type CallbackOnErrorResponse = (error: any) => Promise<any>;
