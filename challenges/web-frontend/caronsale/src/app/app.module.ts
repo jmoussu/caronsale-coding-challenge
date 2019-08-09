d6d62f87db80b6b25be6a7ed249e2e87478c3f39
snntaylan <sinan.taylan@outlook.com>
2019-08-09 23:29:35 +0200

    updated app module

--- challenges/web-frontend/caronsale/src/app/app.module.ts
+++ challenges/web-frontend/caronsale/src/app/app.module.ts
@@ -1,23 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginPageComponent } from "./components/login-page/login-page.component";
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MyMaterialModule } from "./share/my-material.module";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginPageComponent,
-    HomePageComponent
-  ],
+  declarations: [AppComponent, LoginPageComponent, HomePageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MyMaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
