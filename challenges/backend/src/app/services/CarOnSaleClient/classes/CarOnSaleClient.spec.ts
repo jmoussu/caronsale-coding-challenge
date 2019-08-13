a63d1a79b66fb925d2ef877a99c8d90c1e83cb04
Adam Heiderich <adam@Adams-MacBook-Pro.local>
2019-08-13 14:06:48 +0200

    fixed imports order in CarOnSaleClient.spec.ts. This change is necessary for the tests to work propertly

--- challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.spec.ts
+++ challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.spec.ts
@@ -1,22 +1,40 @@
+import "reflect-metadata"
 import { CarOnSaleClient } from "./CarOnSaleClient";
 import { ICarOnSaleClient } from "../interface/ICarOnSaleClient";
-import { AuctionItem } from "../models/AuctionItem";
-import "reflect-metadata";
-
+import assertArrays from "chai-arrays";
 import chai from "chai";
 
 const expect = chai.expect;
 
+
 describe('CarOnSaleClient class test', function () {
     let client: ICarOnSaleClient;
 
-    this.beforeEach(function(){
+    this.beforeAll(function () {
+        chai.use(assertArrays);
+    })
+
+    this.beforeEach(function () {
         client = new CarOnSaleClient();
     })
 
-    it('Should return running auctions', function () {
+    it('Should not return null', function () {
 
-        expect(client.getRunningAuctions()).to.not.throw();
-        
-    })
+        client.getRunningAuctions()
+            .then(auctions => {
+                expect(auctions)
+                    .to.not.be.null
+            });
+
+    });
+
+    it('Should return array of running auctions', function () {
+
+        client.getRunningAuctions()
+            .then(auctions => {
+                expect(auctions)
+                    .to.be.array()
+            });
+
+    });
 });
