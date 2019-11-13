f39364f8ce74dd021234b14ebe7e02e2d9eaad76
jmoussu <jmoussu@student.42.fr>
2019-11-13 22:52:30 +0100

    Add Type to Interface

--- challenges/backend/src/app/services/CarOnSaleClient/interface/ICarOnSaleClient.ts
+++ challenges/backend/src/app/services/CarOnSaleClient/interface/ICarOnSaleClient.ts
@@ -1,8 +1,10 @@
 /**
  * This service describes an interface to access auction data from the CarOnSale API.
  */
+import { AxiosResponse } from "axios";
+
 export interface ICarOnSaleClient {
 
-    getRunningAuctions(): Promise<any /* TODO: Introduce a type */>
+    getRunningAuctions(): Promise<AxiosResponse>;
 
 }
