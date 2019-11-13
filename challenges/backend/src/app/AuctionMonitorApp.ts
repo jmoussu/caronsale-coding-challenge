59918d8abda3f971f5991acbcd5c9893fa0fb102
jmoussu <jmoussu@student.42.fr>
2019-11-13 22:53:56 +0100

    Retrieve auctions and display aggregated information

--- challenges/backend/src/app/AuctionMonitorApp.ts
+++ challenges/backend/src/app/AuctionMonitorApp.ts
@@ -1,20 +1,51 @@
-import {inject, injectable} from "inversify";
-import {ILogger} from "./services/Logger/interface/ILogger";
-import {DependencyIdentifier} from "./DependencyIdentifiers";
+import { inject, injectable } from "inversify";
+import { ILogger } from "./services/Logger/interface/ILogger";
+import { ICarOnSaleClient } from "./services/CarOnSaleClient/interface/ICarOnSaleClient";
+import { DependencyIdentifier } from "./DependencyIdentifiers";
+import { AxiosResponse } from "axios";
 import "reflect-metadata";
 
 @injectable()
 export class AuctionMonitorApp {
 
-    public constructor(@inject(DependencyIdentifier.LOGGER) private logger: ILogger) {
-    }
+    public constructor(
+        @inject(DependencyIdentifier.LOGGER) private logger: ILogger,
+        @inject(DependencyIdentifier.CARONSALECLIENT) private CarOnSaleClient: ICarOnSaleClient) { }
 
     public async start(): Promise<void> {
+        process.on('exit', (code) => {
+            return this.logger.log(`Process EXIT with code ${code}`);
+        });
 
         this.logger.log(`Auction Monitor started.`);
 
-        // TODO: Retrieve auctions and display aggregated information (see README.md)
+        let runningAuctions: AxiosResponse;
+        let data: [];
+        let numberOfAuctions: number;
+        let averageNumberBids: number;
+        let averagePercentageAuctionProgress: number;
 
+        try {
+            runningAuctions = await this.CarOnSaleClient.getRunningAuctions();
+            data = runningAuctions.data;
+            numberOfAuctions = data.length;
+
+            averageNumberBids = 0;
+            averagePercentageAuctionProgress = 0;
+            data.forEach((element: { numBids: number, currentHighestBidValue: number, minimumRequiredAsk: number }) => {
+                averageNumberBids = averageNumberBids + element.numBids;
+                averagePercentageAuctionProgress = averagePercentageAuctionProgress + element.currentHighestBidValue / element.minimumRequiredAsk;
+            });
+            averageNumberBids = averageNumberBids / numberOfAuctions;
+            averagePercentageAuctionProgress = averagePercentageAuctionProgress / numberOfAuctions * 100;
+        } catch (error) {
+            return process.exit(-1);
+        }
+
+        this.logger.log('Number of auctions : ' + numberOfAuctions);
+        this.logger.log('Average number of bids : ' + averageNumberBids);
+        this.logger.log('Average percentage of auction progress, : ' + averagePercentageAuctionProgress.toFixed(2) + '%');
+
+        return process.exit(0);
     }
-
 }
